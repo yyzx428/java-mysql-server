4d32084191f69b532709e36472a55d5e5bdfd1fe
yyzx <zhkun.yin@gmail.com>
2025-03-31 14:46:20 +0800

    init: server handler project struct

---
+++ handler/src/main/java/org/yyzx/mysql/handler/Handler.java
@@ -0,0 +1,7 @@
+package org.yyzx.mysql.handler;
+
+/**
+ * 存储引擎接口
+ */
+public interface Handler {
+}
---
+++ storage/innodb/src/main/java/org/yyzx/mysql/handler/innodb/InnodbHandler.java
@@ -0,0 +1,6 @@
+package org.yyzx.mysql.handler.innodb;
+
+import org.yyzx.mysql.handler.Handler;
+
+public class InnodbHandler implements Handler {
+}
